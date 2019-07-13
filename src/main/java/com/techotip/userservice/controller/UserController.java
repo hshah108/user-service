95ab8bc68201c99d8592c00911aa543eb4ce4419
hardik <hardik0109@gmail.com>
2019-07-13 14:16:36 +0530

    jenkinsfile

--- src/main/java/com/techotip/userservice/controller/UserController.java
+++ src/main/java/com/techotip/userservice/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.techotip.userservice.controller;
 
+import com.techotip.userservice.model.Order;
 import com.techotip.userservice.model.User;
 import com.techotip.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.aggregation.ArrayOperators;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class UserController {
@@ -11,12 +14,15 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @GetMapping("/users/{userId}")
     public User getUserById(@PathVariable Integer userId) {
         return userService.getUserById(userId);
     }
 
-    @PostMapping("users")
+    @PostMapping("/users")
     public User saveUser(@RequestBody User user) {
         return userService.saveUser(user);
     }
@@ -24,5 +30,15 @@ public class UserController {
     @DeleteMapping("/users/{userId}")
     public void deleteUser(@PathVariable Integer userId) {
         userService.deleteUser(userId);
+    }
+
+    @GetMapping("/users/{userId}/orders")
+    public Order getOrder(@PathVariable Integer userId) {
+        return userService.getOrder(userId);
+    }
+
+    @PostMapping("/users/{userId}/orders")
+    public Order putOrder(@PathVariable Integer userId, @RequestBody Order order) {
+        return userService.putOrder(userId, order);
     }
 }
