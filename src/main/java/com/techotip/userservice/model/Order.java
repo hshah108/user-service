f0cd67f88e492750b8cd795a1a94c71f8ffc8404
hardik <hardik0109@gmail.com>
2019-07-13 14:00:09 +0530

    jenkinsfile

---
+++ src/main/java/com/techotip/userservice/model/Order.java
@@ -0,0 +1,17 @@
+package com.techotip.userservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    Integer orderId;
+    String name;
+    String description;
+}
