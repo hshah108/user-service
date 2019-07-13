d3391762fa8062a4a89669a4c7aa41f246a2f4b8
hardik <hardik0109@gmail.com>
2019-07-13 13:58:14 +0530

    jenkinsfile

--- src/main/java/com/techotip/userservice/service/UserService.java
+++ src/main/java/com/techotip/userservice/service/UserService.java
@@ -1,10 +1,13 @@
 package com.techotip.userservice.service;
 
 import com.techotip.userservice.exception.UserNotFoundException;
+import com.techotip.userservice.model.Order;
 import com.techotip.userservice.model.User;
 import com.techotip.userservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
 
@@ -13,6 +16,9 @@ public class UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    RestTemplate restTemplate;
 
     public User getUserById(Integer userId) {
         return userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException(userId));
@@ -24,5 +30,15 @@ public class UserService {
 
     public void deleteUser(Integer userId) {
         userRepository.deleteById(userId);
+    }
+
+    public Order getOrder(Integer userId) {
+        ResponseEntity<Order> responseEntity = restTemplate.getForEntity("http://localhost:8083/orders/user/"+userId, Order.class);
+        return responseEntity.getBody();
+    }
+
+    public Order putOrder(Integer userId, Order order) {
+       ResponseEntity<Order> responseEntity = restTemplate.postForEntity("http://localhost:8083/orders/user"+userId, order, Order.class);
+       return responseEntity.getBody();
     }
 }
